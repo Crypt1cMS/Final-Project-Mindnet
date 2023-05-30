f236a3ab34299e65e2a30f6eac0bcd788d7f5284
Crypt1cMS <dylan.minaya12@gmail.com>
2023-05-29 21:44:40 -0400

    Organized the folders

--- Front-end/Mindnet/src/main.tsx
+++ Front-end/Mindnet/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RegisterPage from './RegisterPage.tsx'
+import SignUp from './Pages/SignUp/SignUp.tsx'
+import App from './Pages/ConfirmationPage/ConfirmationPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RegisterPage />
+    <App />
   </React.StrictMode>,
 )
