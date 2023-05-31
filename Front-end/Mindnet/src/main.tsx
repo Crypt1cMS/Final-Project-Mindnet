ce67a749bfa21ef47e94ec214b4f62aebfde6122
Crypt1cMS <dylan.minaya12@gmail.com>
2023-05-30 23:18:21 -0400

    Added the forgot ForgotPassword page and some other css details

--- Front-end/Mindnet/src/main.tsx
+++ Front-end/Mindnet/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import SignUp from './Pages/SignUp/SignUp.tsx'
-import App from './Pages/ConfirmationPage/ConfirmationPage.tsx'
+// import ConfirmationPage from './Pages/ConfirmationPage/ConfirmationPage.tsx'
+// import LogIn from './Pages/LogIn/LogIn.tsx'
+import ForgotPassword from './Pages/ForgotPassword/ForgotPassword'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ForgotPassword />
   </React.StrictMode>,
 )
