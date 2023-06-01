203e3b5252edaea4c6665b82c44036469f30ef36
Carlos Adames <92492690+KamajiRd@users.noreply.github.com>
2023-05-31 21:03:00 -0400

    Deleted em from divider and added styling in css instead

--- Front-end/Mindnet/src/Pages/ForgotPassword/ForgotPassword.tsx
+++ Front-end/Mindnet/src/Pages/ForgotPassword/ForgotPassword.tsx
@@ -22,7 +22,7 @@ function ForgotPassword() {
                           required
                       />
 
-                      <p id="p-divider"><em>o</em></p>
+                      <p id="p-divider">o</p>
   
                       <input
                           type="text"
