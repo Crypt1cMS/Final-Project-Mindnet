36ebddf45af3ac22d34c3f4877eea8bd7a88bac3
Crypt1cMS <dylan.minaya12@gmail.com>
2023-06-12 22:30:41 -0400

    Removed router and modified paths

--- Front-end/Mindnet/src/main.tsx
+++ Front-end/Mindnet/src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-// import Rutas from './Paths'
+import Rutas from './Paths'
 import { BrowserRouter as Router } from 'react-router-dom';
-import NewPassword from './Pages/New Password/Newpassword'
+// import NewPassword from './Pages/New Password/Newpassword'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <NewPassword />
+      <Rutas />
     </Router>
   </React.StrictMode>,
 )
