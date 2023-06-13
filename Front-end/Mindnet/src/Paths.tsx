748fd8058bbc50569cd6cddfb597570dee522727
gustavo <92757784+gustavo2804@users.noreply.github.com>
2023-06-12 22:31:00 -0400

    Agregada redireccion al login

--- Front-end/Mindnet/src/Paths.tsx
+++ Front-end/Mindnet/src/Paths.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes} from 'react-router-dom'
+import { Route, Routes, Navigate} from 'react-router-dom'
 import SignUp from './Pages/Sign Up/SignUp.tsx'
 import LogIn from './Pages/Log In/LogIn.tsx'
 import ConfirmationPage from './Pages/Confirmation Page/ConfirmationPage.tsx'
@@ -9,7 +9,8 @@ function Rutas () {
         
         <Routes>
             <Route path ='/signup' element={<SignUp />} />
-            <Route path = '/' element={<LogIn/>} />
+            <Route path = '/login' element={<LogIn/>} />
+            <Route path = '/*' element={<Navigate to="/login"/>} />
             <Route path = '/ConfirmationPage' element={<ConfirmationPage/>} />
             <Route path = '/ForgotPassword' element={<ForgotPassword/>} />
         </Routes>
