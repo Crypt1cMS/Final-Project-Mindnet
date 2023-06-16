29ea0c74f7350bbe864022cbded37f112b1e842e
unknown <adamescarlos2808@gmail.com>
2023-06-16 13:53:45 -0400

    Modified Post Component to take pictures and user name for posts

--- Front-end/Mindnet/src/Pages/components/post.tsx
+++ Front-end/Mindnet/src/Pages/components/post.tsx
@@ -1,58 +1,94 @@
-import '../../global.css'
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-import { useState } from 'react';
+interface Comment {
+  id: number;
+  text: string;
+  user: {
+    name: string;
+    profilePicture: string;
+  };
+}
 
-const SocialMediaPost = ({ image, likes, comments, shares }) => {
-    const [commentText, setCommentText] = useState('');
-    const [postComments, setPostComments] = useState(comments);
-  
-    const handleCommentChange = (event:any) => {
-      setCommentText(event.target.value);
-    };
-  
-    const handleCommentSubmit = (event:any) => {
-      event.preventDefault();
-      if (commentText !== '') {
-        const newComment = {
-          id: postComments.length + 1,
-          text: commentText
-        };
-        setPostComments([...postComments, newComment]);
-        setCommentText('');
-      }
-    };
-  
-    return (
-      <div id="posts-wrapper">  
-        <div className="social-media-post-card">
-            <img className="post-image" src={image} alt="Post" />
+interface SocialMediaPostProps {
+  image: string;
+  likes: number;
+  comments: Comment[];
+  shares: number;
+}
 
-            <div className="post-buttons">
-                <button>{likes} Like</button>
-                <button>{comments.length} Comment</button>
-                <button>{shares} Share</button>
+const SocialMediaPost: React.FC<SocialMediaPostProps> = ({
+  image,
+  likes,
+  comments,
+  shares,
+}) => {
+  const [commentText, setCommentText] = useState('');
+  const [postComments, setPostComments] = useState<Comment[]>(comments);
+
+  const handleCommentChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCommentText(event.target.value);
+  };
+
+  const handleCommentSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (commentText !== '') {
+      const newComment: Comment = {
+        id: postComments.length + 1,
+        text: commentText,
+        user: {
+          name: 'John Doe', // Replace with the user's name
+          profilePicture: 'profile-picture.jpg', // Replace with the user's profile picture URL
+        },
+      };
+      setPostComments([...postComments, newComment]);
+      setCommentText('');
+    }
+  };
+
+  return (
+    <div id="posts-wrapper">
+      <div className="social-media-post-card">
+        <img className="post-image" src={image} alt="Post" />
+
+        <div className="post-buttons">
+          <button>{likes} Like</button>
+          <button>{comments.length} Comment</button>
+          <button>{shares} Share</button>
+        </div>
+
+        <div className="comment-section">
+          <form onSubmit={handleCommentSubmit}>
+            <input
+              className="comment-input"
+              type="text"
+              placeholder="Add a comment..."
+              value={commentText}
+              onChange={handleCommentChange}
+            />
+            <button className="comment-button" type="submit">
+              <img src="../../../public/send-2-svgrepo-com.svg" alt="" />
+            </button>
+          </form>
+        </div>
+
+        <div className="post-comments">
+          {postComments.map((comment) => (
+            <div key={comment.id}>
+              <img
+                className="comment-profile-picture"
+                src={comment.user.profilePicture}
+                alt="Profile"
+              />
+              <p>
+                <span className="comment-username">{comment.user.name}</span>{' '}
+                {comment.text}
+              </p>
             </div>
-
-            <div className="comment-section">
-            <form onSubmit={handleCommentSubmit}>
-                <input
-                className="comment-input"
-                type="text"
-                placeholder="Add a comment..."
-                value={commentText}
-                onChange={handleCommentChange}
-                />
-                <button className="comment-button" type="submit"><img src="../../../public/send-2-svgrepo-com.svg" alt="" /></button>
-            </form>
-            </div>
-            <div className="post-comments">
-            {postComments.map((comment:any) => (
-                <p key={comment.id}>{comment.text}</p>
-            ))}
-            </div>
+          ))}
         </div>
       </div>
-    );
-  };
-  
-  export default SocialMediaPost;
+    </div>
+  );
+};
+
+export default SocialMediaPost;
