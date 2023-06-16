0c0f1d6c80c87509fd6541f8ccf3d8dc62d3603d
Crypt1cMS <dylan.minaya12@gmail.com>
2023-06-16 01:59:05 -0400

    Created the side bar

--- Front-end/Mindnet/src/main.tsx
+++ Front-end/Mindnet/src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 // import  Rutas from './Paths';
-import Sidebar from './Pages/Sidebar/Sidebar'
+import Sidebar from './Pages/components/Sidebar'
+// import NavBar from './Pages/components/navbar'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
