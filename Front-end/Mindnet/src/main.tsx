6d25d5adaa9f04f997496c3611066a439634cd62
Crypt1cMS <dylan.minaya12@gmail.com>
2023-06-16 21:45:33 -0400

    Added/finished de 404 component

--- Front-end/Mindnet/src/main.tsx
+++ Front-end/Mindnet/src/main.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 // import  Rutas from './Paths';
-import Sidebar from './Pages/components/Sidebar'
-// import NavBar from './Pages/components/navbar'
-
+import Error404 from './Pages/404 Error/Error404'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <Sidebar />
+      <Error404 />
     </Router>
   </React.StrictMode>,
 )
