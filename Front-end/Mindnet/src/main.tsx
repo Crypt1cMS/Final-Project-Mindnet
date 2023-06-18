e49dec6ccb18ed17b0a258b7a8c6745baed9a4c2
Crypt1cMS <dylan.minaya12@gmail.com>
2023-06-17 23:02:56 -0400

    Added the Password Change and Password Change-Nav

--- Front-end/Mindnet/src/main.tsx
+++ Front-end/Mindnet/src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 // import  Rutas from './Paths';
-import Error404 from './Pages/404 Error/Error404'
+// import Error404 from './Pages/404 Error/Error404'
+import PasswordChange from './Pages/Password-Change/Password-Change'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <Error404 />
+      <PasswordChange/>
     </Router>
   </React.StrictMode>,
 )
