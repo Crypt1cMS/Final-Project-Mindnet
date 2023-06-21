a1f684236b68e590a87c3fec483f17b8ba0ad9ac
Crypt1cMS <dylan.minaya12@gmail.com>
2023-06-20 21:59:46 -0400

    Fixed feed

--- Front-end/Mindnet/src/Pages/Feed/FeedPage.tsx
+++ Front-end/Mindnet/src/Pages/Feed/FeedPage.tsx
@@ -1,6 +1,6 @@
 import '../../global.css'
-import Sidebar from '../components/Sidebar'
-import NavBar from '../components/navbar'
+// import Sidebar from '../components/Sidebar'
+// import NavBar from '../components/navbar'
 import './FeedStyle.css'
 
 export const Feed = () => {
