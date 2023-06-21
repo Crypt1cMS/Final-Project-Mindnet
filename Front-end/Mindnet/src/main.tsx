a40cfab5b72530f1f5860705ee17791dba098e82
Crypt1cMS <dylan.minaya12@gmail.com>
2023-06-21 19:39:48 -0400

    Finished the terms and condition for Mindnet

--- Front-end/Mindnet/src/main.tsx
+++ Front-end/Mindnet/src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
-import  Rutas from './Paths';
+// import  Rutas from './Paths';
 // import NavBar from './Pages/components/navbar';
-// import TerminosCondiciones from './Pages/TerminosCondiciones/TerminosCondiciones'
+import TerminosCondiciones from './Pages/TerminosCondiciones/TerminosCondiciones'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <Rutas/>
+      <TerminosCondiciones/>
     </Router>
   </React.StrictMode>,
 )
